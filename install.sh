eb30108d049641ff3d8314edf9f30ac620e99c75
khiem11 <128479805+khiem11@users.noreply.github.com>
2025-05-10 10:54:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,14 +6,4 @@ yes | pkg install libjansson nano git
 git clone https://github.com/khiem11/ccminer.git
 chmod +x ccminer/start.sh ccminer/ccminer
 
-mkdir -p ~/.termux/boot
-
-cat > ~/.termux/boot/termux.sh << 'EOF'
-#!/data/data/com.termux/files/usr/bin/sh
-termux-wake-lock
-~/ccminer/start.sh
-EOF
-
-chmod +x ~/.termux/boot/termux.sh
-
 echo "✅ Cài đặt hoàn tất! Hãy đảm bảo bạn đã cài app Termux:Boot."
